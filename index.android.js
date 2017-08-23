a4a1946fbb2700eb2814f696ef02b983c4e69724
gitjzu <developer.yacine@gmail.com>
2017-08-23 23:41:09 +0300

    Add Routing. Change Navigation bar to routers own implementation

--- index.android.js
+++ index.android.js
@@ -8,9 +8,16 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Platform
 } from 'react-native'
+import { NativeRouter, Link } from 'react-router-native'
+import { Navigation, Card } from 'react-router-navigation'
+import Icon from 'react-native-vector-icons/Ionicons';
 import { api } from './configs/config.js'
+import DetailedSpot from './jsx/DetailedSpot'
+import MenuButton from './jsx/MenuButton'
+import Drawer from './jsx/Drawer'
 import App from './jsx/App'
 
 export default class ParkingSpots extends Component {
@@ -25,13 +32,50 @@ export default class ParkingSpots extends Component {
   render() {
     return (
       <ApolloProvider client={this.createClient()}>
-        <View style={styles.container}>
-          <App />
-        </View>
+        <NativeRouter>
+          <Drawer>
+            { Platform.OS === 'android' && Platform.Version >= 20 ?
+            <View
+              style={{
+                height: 24,
+                backgroundColor: "#304ffe",
+              }}
+            />
+            : null }
+            <Navigation
+              navBarStyle={navBarStyles.navBar}
+              titleStyle={navBarStyles.title}
+              backButtonTintColor='white'
+              backButtonTitle='Takaisin'
+              title='Parking Spots'
+            >
+              <Card
+                style={styles.container}
+                exact
+                path="/"
+                component={App}
+                renderLeftButton={() => <MenuButton/>}
+              />
+              <Card
+                path="/spot"
+                component={DetailedSpot}
+              />
+            </Navigation>
+          </Drawer>
+        </NativeRouter> 
       </ApolloProvider>
     )
   }
 }
+
+const navBarStyles = StyleSheet.create({
+  navBar: {
+    backgroundColor: '#304ffe',
+  },
+  title: {
+    color: 'white'
+  }
+})
 
 const styles = StyleSheet.create({
   container: {
