c778ecdbb4567a36525d052f92d39cf46c5c47db
gitjzu <developer.yacine@gmail.com>
2017-08-27 21:02:22 +0300

    Remove console.log statements

--- jsx/util.js
+++ jsx/util.js
@@ -19,13 +19,10 @@ export function regionFrom(lat, lon, distance = 300 ) {
 
 //Placeholder for getting some kind of distance. This returns the crow fly path 
 export function distance(lat1, lon1, lat2, lon2) {
-  console.log('inside distance function')
   var p = 0.017453292519943295    // Math.PI / 180
   var c = Math.cos
   var a = 0.5 - c((lat2 - lat1) * p)/2 + 
           c(lat1 * p) * c(lat2 * p) * 
           (1 - c((lon2 - lon1) * p))/2
-
-  console.log('distance: ' + 12742 * Math.asin(Math.sqrt(a)))
   return 12742 * Math.asin(Math.sqrt(a)) // 2 * R; R = 6371 km
 }
